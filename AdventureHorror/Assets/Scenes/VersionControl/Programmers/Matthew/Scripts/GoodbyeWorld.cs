9f4627817b05ee6f70383cd11d83d251a394a6c4
CornKernel <88573972+CornKerne1@users.noreply.github.com>
2021-10-15 23:32:12 -0700

    Goodbye World Script

---
+++ AdventureHorror/Assets/Scenes/VersionControl/Programmers/Matthew/Scripts/GoodbyeWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodbyeWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Goodbye World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
