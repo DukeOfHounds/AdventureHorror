3ec1893044660c1bc3df609421003d72323e2bf7
Connor Hartzog <hartzogcr@gmail.com>
2021-10-16 22:32:50 -0700

    Learned Culling and Rotating Lights

--- AdventureHorror/Assets/Scenes/VersionControl/Programmers/Connor/Scripts/Junk/SpinTowards.cs
+++ AdventureHorror/Assets/Scenes/VersionControl/Programmers/Connor/Scripts/Junk/SpinTowards.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class SpinTowards : MonoBehaviour
 {
+
+ //   private GameObject obj = GameObject.Find("Main Camera");
+
     public float smoothing = 1f;// speed of spin
-    
+
+    private List<Target> targets;
     public Vector3 Target
     {
-        get { return target;  } 
+        get { return target; }
         set
         {
             target = value;
@@ -21,11 +25,24 @@ public class SpinTowards : MonoBehaviour
     private Vector3 target;
     IEnumerator Spin(Vector3 target)
     {
-        while (Vector3.Angle(gameObject.transform.forward, target) != 0) 
-        {
-            transform.LookAt(target);
-            yield return null;
-        }
-        
+        Debug.Log("turning");
+        //while (Vector3.Angle(gameObject.transform.forward, target) != 0) 
+        //{
+        Vector3 direction = (target - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, direction.y, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * smoothing);
+        yield return null;
+        //}
+
     }
+}
+
+public class Target
+{
+    Vector3 target;
+    public Target(Vector3 target)
+    {
+        this.target = target;
+    }
+
 }
