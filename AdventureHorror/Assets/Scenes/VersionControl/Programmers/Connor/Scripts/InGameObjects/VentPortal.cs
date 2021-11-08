6ac9c9c690529e17b265a4bba88b09839a3ea3be
Connor Hartzog <hartzogcr@gmail.com>
2021-11-07 17:44:36 -0800

    Teleportation but no rotation on vents

--- AdventureHorror/Assets/Scenes/VersionControl/Programmers/Connor/Scripts/InGameObjects/VentPortal.cs
+++ AdventureHorror/Assets/Scenes/VersionControl/Programmers/Connor/Scripts/InGameObjects/VentPortal.cs
@@ -7,8 +7,34 @@ public class VentPortal : MonoBehaviour
     public VentPortal VentOfExit;
     public Transform ExitPosition;
     public PlayerData PD;
+
+    private static bool TP = true;
     public void Teleport()
     {
-        PD.player.transform.position = VentOfExit.ExitPosition.transform.position;// teleports player
+        if (TP == true)
+        {
+            Debug.Log("woosh woosh woosh");
+            TP = false;
+            StartCoroutine(TeleportPosition(VentOfExit.ExitPosition, 1f));
+        }
+        //PD.player.transform.position = VentOfExit.ExitPosition.transform.position;// teleports player
+        //PD.player.gameObject.transform.position = new Vector3(VentOfExit.ExitPosition.transform.position.x, VentOfExit.ExitPosition.transform.position.y, VentOfExit.ExitPosition.transform.position.z);
+    }
+    IEnumerator TeleportPosition(Transform targetPosition, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = PD.player.gameObject.transform.position;
+
+        while (time < duration)
+        {
+            PD.player.gameObject.transform.position = targetPosition.position;
+            //PD.player.gameObject.transform.rotation = new Quaternion(PD.player.gameObject.transform.rotation.x,
+            //    VentOfExit.transform.rotation.y, PD.player.gameObject.transform.rotation.z, PD.player.gameObject.transform.rotation.w);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        PD.player.gameObject.transform.position = targetPosition.position;
+        PD.player.gameObject.transform.position = PD.player.gameObject.transform.position;
+        TP = true;
     }
 }
