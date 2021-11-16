7647d7c513e7e44d80a685c18033a5aa46a2e904
CornKernel <88573972+CornKerne1@users.noreply.github.com>
2021-11-15 22:49:35 -0800

    Papa PUSH!

--- AdventureHorror/Assets/Scenes/VersionControl/Programmers/Matthew/Scripts/Papa/Components/PP_Movement.cs
+++ AdventureHorror/Assets/Scenes/VersionControl/Programmers/Matthew/Scripts/Papa/Components/PP_Movement.cs
@@ -94,34 +94,41 @@ public class PP_Movement
 
     private void Search()
     {
-        if (papa.agent.velocity.magnitude < 1f)
-        { 
-            papa.agent.SetDestination(papa.pD.player.gameObject.transform.position);
-            currentState = State.StartSearch;
-        }
-        else if (searchNodes.Count == 0 && timesSearched > 0)
+        //if (papa.agent.velocity.magnitude < 1f)
+        //{ 
+        //    papa.agent.SetDestination(papa.pD.player.gameObject.transform.position);
+        //    currentState = State.StartSearch;
+        //}
+        //else if
+        if (searchNodes.Count == 0 && timesSearched > 3)
         {
-            StartSearch();
+            Debug.Log("Hello1");
+            currentState = State.StartSearch;
             timesSearched = 0;
         }
         else if (searchNodes.Count == 0)
         {
+            Debug.Log("Hello2");
             searching = true;
             papa.agent.speed = papaData.papaBaseSpeed;
             count = Physics.OverlapSphereNonAlloc(paparef.transform.position, 30f, colliders, papaData.searchNodeLayer, QueryTriggerInteraction.Collide);
             for (int i = 0; i < count; ++i)
             {
+                Debug.Log("Hello3");
                 if (papaData.canSeeTarget)
                 {
+                    Debug.Log("Hello4");
                     break;
                 }
                 else
                 {
+                    Debug.Log("Hello5");
                     GameObject obj = colliders[i++].gameObject;
 
                     Vector3 distanceToNode = paparef.transform.position - obj.transform.position;
                     if (distanceToNode.magnitude > 3)
                     {
+                        Debug.Log("Hello6");
                         searchNodes.Add(obj);
                     }
                 }
@@ -132,11 +139,11 @@ public class PP_Movement
 
         else
         {
+            Debug.Log("Hello7");
             int random = Mathf.Abs(Random.Range(0, searchNodes.Count - 1));
             if (cSNode == null)
             {
-                ++papaData.timesSearched;
-                ++timesSearched;
+                Debug.Log("Hello8");
                 index = random;
                 cSNode = searchNodes[index];
                 papaData.currentDest = cSNode.transform.position;
@@ -144,12 +151,30 @@ public class PP_Movement
             }
             else
             {
-                Vector3 distanceToNode = paparef.transform.position - papaData.currentDest;
+                Debug.Log("Hello9");
+                Vector3 distanceToNode = papa.agent.transform.position - papaData.currentDest;
                 if (distanceToNode.magnitude < 1.5)
                 {
+                    ++papaData.timesSearched;
+                    ++timesSearched;
+                    Debug.Log("Hello10");
                     searchNodes.RemoveAt(index);
                     cSNode = null;
                     papa.StopMovement(2f);
+                }
+                else
+                {
+                    Vector3 distanceToPlayer = papa.agent.transform.position - papa.pD.player.gameObject.transform.position;
+                    if (distanceToPlayer.magnitude > 15 && !papaData.canSeeTarget)
+                    {
+                        papaData.currentDest = cSNode.transform.position;
+                        papa.agent.SetDestination(papaData.currentDest);
+                    }
+                    else
+                    {
+                        papaData.currentDest = papa.pD.player.gameObject.transform.position;
+                        currentState = State.Chase;
+                    }
                 }
             }
         }
