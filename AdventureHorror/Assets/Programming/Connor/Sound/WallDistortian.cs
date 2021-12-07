2f5a57698de0ebd03171f21151988037ee947d11
Connor Hartzog <hartzogcr@gmail.com>
2021-12-07 10:10:31 -0800

    I don't remember

--- AdventureHorror/Assets/Programming/Connor/Sound/WallDistortian.cs
+++ AdventureHorror/Assets/Programming/Connor/Sound/WallDistortian.cs
@@ -16,17 +16,16 @@ public class WallDistortian : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        papa = GameObject.FindGameObjectWithTag("Papa");
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = player.GetComponent<Player>().hand.gameObject;
+        //papa = GameObject.FindGameObjectWithTag("Papa");
+        //player = GameObject.Find("hand");
     }
-
     // Update is called once per frame
     void Update()
     {
         Ray ray = new Ray(papa.transform.position, player.transform.position);
         RaycastHit hit;// holds data on what is infront of the player
-        Debug.DrawRay(papa.transform.position, player.transform.position);
+        Debug.DrawRay(papa.gameObject.transform.position, player.gameObject.transform.position);
         Physics.Raycast(ray, out hit);// finds what is infront of the player
         if (hit.collider != null && hit.collider.gameObject == player)
         {
